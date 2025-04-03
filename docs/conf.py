ddde4918d8946fadc48f52664e092f5ac1609350
Ayana Tamu Arce <ath11@duke.edu>
2025-04-02 21:35:53 -0400

    tewak

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,6 @@ html_logo = "_static/ttbar.png"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-html_theme = 'pyramid'
 # Add any paths that contain templates here, relative to this directory.
 #templates_path = ["_templates"]
 
@@ -45,9 +44,10 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 #
 #html_theme = "sphinx_rtd_theme"
-
+html_theme = 'pyramid'
 #html_theme_options = {"logo_only": True}
-html_theme = "click"
+
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
